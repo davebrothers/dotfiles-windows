09b1d641de3236e46a9958c12bdfa83fb90324c3
Dave Brothers <brothers.dave@gmail.com>
2019-02-28 08:44:20 -0500

    Add powertools installer

--- install.ps1
+++ install.ps1
@@ -2,33 +2,37 @@
 
 #################################
 #          .gitconfig
+#
 #################################
 
 if (!(Test-Path $HOME\.gitconfig)) {
-   copyGitConfig
+  copyGitConfig
 
-} else {
-    $gitconfigChoice = Read-Host "Global .gitconfig exists. Replace with dotfiles version? (Y/n)"
-    if (!$gitconfigChoice -eq 'n' -and !$gitconfigChoice -eq 'N') {
-        copyGitConfig
-    } else {
-        Write-Host -ForegroundColor Yellow ".gitconfig will not be replaced.`n"
-    }
+}
+else {
+  $gitconfigChoice = Read-Host "Global .gitconfig exists. Replace with dotfiles version? (Y/n)"
+  if ($gitconfigChoice -ne "n" -and $gitconfigChoice -ne "N") {
+    copyGitConfig
+  }
+  else {
+    Write-Host -ForegroundColor Yellow ".gitconfig will not be replaced.`n"
+  }
 }
 
 Function copyGitConfig {
-    Write-Host -ForegroundColor Yellow "Copying global .gitconfig..."
-    $gitUsername = Read-Host "Git username: "
-    $gitEmail = Read-Host "Git email address: "
+  Write-Host -ForegroundColor Yellow "Copying global .gitconfig..."
+  $gitUsername = Read-Host "Git username: "
+  $gitEmail = Read-Host "Git email address: "
 
-    try {
-        Copy-Item .\.gitconfig $HOME\.gitconfig -ErrorAction 'Stop'
-        Add-Content -Path $HOME\.gitconfig -Value ("`r`n[user]`r`n  name = $($gitUsername)`r`n  email = $($gitEmail)") -ErrorAction 'Stop'
-        Write-Host -ForegroundColor Green ".gitconfig installed at $($HOME)\.gitconfig`n"
-    } catch {
-        Write-Host -ForegroundColor Red "An error occurred installing .gitconfig:"
-        Write-Host "$_`n";
-    }
+  try {
+    Copy-Item .\.gitconfig $HOME\.gitconfig -ErrorAction 'Stop'
+    Add-Content -Path $HOME\.gitconfig -Value ("`r`n[user]`r`n  name = $($gitUsername)`r`n  email = $($gitEmail)") -ErrorAction 'Stop'
+    Write-Host -ForegroundColor Green ".gitconfig installed at $($HOME)\.gitconfig`n"
+  }
+  catch {
+    Write-Host -ForegroundColor Red "An error occurred installing .gitconfig:"
+    Write-Host "$_`n";
+  }
 }
 
 #################################
@@ -37,19 +41,38 @@ Function copyGitConfig {
 #################################
 
 if ($env:Path.ToLower().Contains("microsoft vs code")) {
-    vsCodeSettings
-} else {
-    Write-Host -ForegroundColor Red "Could no find code in PATH. Either VS Code is not installed or has  not been added to your PATH."
+  vsCodeSettings
+}
+else {
+  Write-Host -ForegroundColor Red "Could no find code in PATH. Either VS Code is not installed or has  not been added to your PATH."
 }
 
 Function vsCodeSettings {
-    $settingsSyncExtensionName = "Shan.code-settings-sync"
-    $settingsSyncInstalled = code --list-extensions | ? { $_ -eq $settingsSyncExtensionName }
+  $settingsSyncExtensionName = "Shan.code-settings-sync"
+  $settingsSyncInstalled = code --list-extensions | ? { $_ -eq $settingsSyncExtensionName }
 
-    if (![String]::IsNullOrEmpty($settingsSyncExtensionName)) {
-        Write-Host -ForegroundColor Yellow "VS Code Extension $settingsSyncExtensionName is installed.`nUse it to manage user settings.`n"
-    } else {
-        Write-Host -ForegroundColor Yellow "Installing VS Code extension $settingsSyncExtensionName..."
-        code --install-extension "$settingsSyncExtensionName"
+  if (![String]::IsNullOrEmpty($settingsSyncInstalled)) {
+    Write-Host -ForegroundColor Yellow "VS Code Extension $settingsSyncExtensionName is installed.`nUse it to manage user settings.`n"
+  }
+  else {
+    Write-Host -ForegroundColor Yellow "Installing VS Code extension $settingsSyncExtensionName..."
+    code --install-extension "$settingsSyncExtensionName"
+  }
+}
+
+#################################
+#          POWERTOOLS
+#    
+#################################
+$powertoolsInstallLocation = "$env:HOME\Documents\WindowsPowerShell"
+
+if (!Get-Module "davebrothers.powertools") {
+  Write-Host -ForegroundColor Yellow "Could not find davebrothers.powertools."
+  $installPowerToolsChoice = Read-Host "Clone powertools into $powertoolsInstallLocation? (Y/n)"
+  if ($installPowerToolsChoice -ne "N" -and $installPowerToolsChoice -ne "n") {
+    if (Test-Path $powertoolsInstallLocation) {
+      Remove-Item $powertoolsInstallLocation
     }
+    git clone git@github.com:davebrothers/PowerTools.git $powertoolsInstallLocation
+  }
 }
